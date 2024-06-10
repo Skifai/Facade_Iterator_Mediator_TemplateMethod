2fc9c46fd2e561a877617f7379ef26fdb03e3410
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2024-06-10 18:46:58 +0200

    Implementet Facadepattern

---
+++ tests/Facade/FacadeTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.Facade;
+using FluentAssertions;
+
+namespace DesignPatterns.Tests.Facade
+{
+    public class FacadeTests
+    {
+        [Fact]
+        public void NoDiscount()
+        {
+            // Arrange
+            var facade = new StrategyFacade();
+            var sale = new Sale(100, facade);
+
+            // Act
+            var result = sale.GetTotal();
+
+            // Asset
+            result.Should().Be(100);
+        }
+
+        [Fact]
+        public void PercentageDiscount()
+        {
+            // Arrange
+            var facade = new StrategyFacade();
+            var sale = new Sale(110, facade);
+
+            // Act
+            var result = sale.GetTotal();
+
+            // Asset
+            result.Should().Be(99);
+        }
+    }
+}
