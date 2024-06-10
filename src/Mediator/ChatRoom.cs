25544cf015b114492d79e072c3585deb874f69e8
Luca1096 <133788179+Luca1096@users.noreply.github.com>
2024-06-10 18:52:35 +0200

    Mediator

---
+++ src/Mediator/ChatRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Mediator
+{
+    public class ChatRoom : IChatRoomMediator
+    {
+        public void ShowMessage(User user, string message)
+        {
+            string time = DateTime.Now.ToString("HH:mm:ss");
+            string sender = user.GetName();
+
+            Console.WriteLine($"{time} [{sender}]: {message}");
+        }
+    }
+
+}
